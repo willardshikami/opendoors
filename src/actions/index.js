cd77cbfb3968a84fa3926e61966c0ef6473c6430
willardshikami <willardshikami@gmail.com>
2018-03-31 04:48:50 +0300

    created reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,11 @@ import * as types from '../constants/ActionTypes'
 let nextMessageId = 0;
 const nextUserId = 0;
 
-export const addMessage = (message, author) => ({type: types.ADD_MESSAGE,
-id: nextMessageId++,
-message,
-author
+export const addMessage = (message, author) => ({
+    type: types.ADD_MESSAGE,
+    id: nextMessageId++,
+    message,
+    author
 })
 
 export const addUser = name => ({
