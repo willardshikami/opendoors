e6e6edcdf734172db6fc8f812093197b4755dff1
willardshikami <willardshikami@gmail.com>
2018-04-03 12:59:33 +0300

    added user visibility to sidebar

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { addUser } from './actions';
 
 import  chat from './reducers'
 
 const store = createStore(chat)
+
+store.dispatch(addUser('Me'))
 
 ReactDOM.render(
     <Provider store={store}>
