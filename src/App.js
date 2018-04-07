ee36b5bd8623c0ac2e8f33a1e06b66ade6c51769
willardshikami <willardshikami@gmail.com>
2018-04-07 14:16:59 +0300

    created the server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import  { Sidebar } from './containers/Sidebar';
 import  { MessagesList } from './containers/MessagesList';
